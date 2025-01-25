8374dc7c8ca9432d68da06df3296dccef04ce5ae
sadmansakib452 <sadmansakib452@gmail.com>
2025-01-25 14:28:56 +0600

    added MONgO_URI variable in db drectly

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 const config = require("../config/config");
 
 const connectDB = async () => {
+
+
+  const MONGO_URI =
+    "mongodb+srv://sadmansakib452:i5vpVM6hBRe4uEJn@cluster0.ghkwfvp.mongodb.net/dream-radio-db?retryWrites=true&w=majority&appName=Cluster0";
   try {
-    const conn = await mongoose.connect(config.mongoUri);
+    const conn = await mongoose.connect(MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
